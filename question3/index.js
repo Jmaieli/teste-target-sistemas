6c8ad43a8449e05c7066b6f3110dd87803571ce7
Jmaieli <jmaielisribeiro@gmail.com>
2025-01-31 12:31:17 -0300

    corrigindo a leitura do arquivo Json

--- question3/index.js
+++ question3/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
+const { exit } = require("process");
 
-const rawData = fs.readFileSync("dados.json", "utf8");
+const rawData = fs.readFileSync(`${__dirname}/dados.json`, "utf8");
 
 const dados = JSON.parse(rawData);
 
@@ -8,16 +9,17 @@ const faturamentosValidos = dados.filter((item) => item.valor > 0);
 
 if (faturamentosValidos.length === 0) {
   console.log("Não há faturamento registrado para análise.");
-} else {
-  const menorFaturamento = Math.min(...faturamentosValidos.map((item) => item.valor));
-
-  const maiorFaturamento = Math.max(...faturamentosValidos.map((item) => item.valor));
-
-  const formatador = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  console.log(`Menor faturamento diário do mês: ${formatador.format(menorFaturamento)}`);
-  console.log(`Maior faturamento diário do mês: ${formatador.format(maiorFaturamento)}`);
+  exit(0);
 }
+
+const menorFaturamento = Math.min(...faturamentosValidos.map((item) => item.valor));
+
+const maiorFaturamento = Math.max(...faturamentosValidos.map((item) => item.valor));
+
+const formatador = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
+console.log(`Menor faturamento diário do mês: ${formatador.format(menorFaturamento)}`);
+console.log(`Maior faturamento diário do mês: ${formatador.format(maiorFaturamento)}`);
